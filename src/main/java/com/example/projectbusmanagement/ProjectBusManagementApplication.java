5b14294d54935bd7d5ace59f613c16384266be51
nguyenxuandat252 <nguyenxuandat252@gmail.com>
2025-03-26 09:44:18 +0700

    first commit

---
+++ src/main/java/com/example/projectbusmanagement/ProjectBusManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectbusmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBusManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBusManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectbusmanagement/ProjectBusManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectbusmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBusManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
